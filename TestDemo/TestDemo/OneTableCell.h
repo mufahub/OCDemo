f722dde13f4d1883a38b650ed897dba6eef1b0dc
MF <710371095@qq.com>
2018-05-03 15:39:51 +0800

    添加OneCell

---
+++ TestDemo/TestDemo/OneTableCell.h
@@ -0,0 +1,14 @@
+//
+//  TwoTableCell.h
+//  TestDemo
+//
+//  Created by MF on 18/5/3.
+//  Copyright © 2018年 MF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OneTableCell : UITableViewCell
+
+@property (nonatomic, copy) NSString *name;
+@end
